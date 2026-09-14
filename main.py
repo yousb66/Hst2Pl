a319ef73cc09c9665d42c086daffd93492e54694
yousb66 <siwete@protonmail.com>
2026-09-14 08:25:08 +0000

    Update renewal URL in main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=8bd2d929-2c8d-4970-9292-f62b2fad6a31",
     # 添加更多链接
 ]
 
